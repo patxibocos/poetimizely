a404b60d099f2ca996ed362b3e743212b3b733e2
Patxi Bocos <patxi952@gmail.com>
2020-03-25 07:51:37 -0700

    Fixing conflicts

--- src/main/kotlin/com/patxi/poetimizely/plugin/GeneratorTask.kt
+++ src/main/kotlin/com/patxi/poetimizely/plugin/GeneratorTask.kt
@@ -1,8 +1,8 @@
 package com.patxi.poetimizely.plugin
 
 import com.patxi.poetimizely.generator.Generator
-import com.patxi.poetimizely.optimizely.OptimizelyService
-import com.patxi.poetimizely.optimizely.buildOptimizelyService
+import com.patxi.poetimizely.optimizely.ExperimentsService
+import com.patxi.poetimizely.optimizely.buildExperimentsService
 import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -14,7 +14,7 @@ open class GeneratorTask : DefaultTask() {
 
     @TaskAction
     fun doAction() {
-        val service: OptimizelyService = buildOptimizelyService(requireNotNull(optimizelyToken))
+        val service: ExperimentsService = buildExperimentsService(requireNotNull(optimizelyToken))
         val generator = Generator()
         runBlocking {
             val experiments = service.listExperiments(requireNotNull(optimizelyProjectId))
