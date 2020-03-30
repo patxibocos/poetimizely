7e24dcb0075a99dfbdbdcae10d996b07ac3f3769
Patxi Bocos <patxi952@gmail.com>
2020-03-30 10:04:54 -0700

    Adding packageName to the plugin extension

--- src/main/kotlin/com/patxi/poetimizely/plugin/GeneratorTask.kt
+++ src/main/kotlin/com/patxi/poetimizely/plugin/GeneratorTask.kt
@@ -1,9 +1,12 @@
 package com.patxi.poetimizely.plugin
 
 import com.patxi.poetimizely.generator.ExperimentsGenerator
+import com.patxi.poetimizely.generator.FeaturesGenerator
 import com.patxi.poetimizely.optimizely.ExperimentsService
+import com.patxi.poetimizely.optimizely.FeaturesService
 import com.patxi.poetimizely.optimizely.authenticatedRetrofit
 import com.patxi.poetimizely.optimizely.buildExperimentsService
+import com.patxi.poetimizely.optimizely.buildFeaturesService
 import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
@@ -11,21 +14,33 @@ import org.gradle.api.tasks.TaskAction
 open class GeneratorTask : DefaultTask() {
     var optimizelyProjectId: Long? = null
     var optimizelyToken: String? = null
+    var packageName: String? = null
 
     @TaskAction
     fun doAction() {
         val optimizelyProjectId = optimizelyProjectId
         val optimizelyToken = optimizelyToken
-        if (optimizelyProjectId == null || optimizelyToken == null) {
-            logger.error("Skipping generator task as missing required arguments")
+        val packageName = packageName
+        if (optimizelyProjectId == null || optimizelyToken == null || packageName == null) {
+            logger.error(
+                """
+                    |Skipping generator task as missing required arguments:
+                    |- optimizelyProjectId
+                    |- optimizelyToken
+                    |- packageName""".trimMargin()
+            )
             return
         }
         val authenticatedRetrofit = authenticatedRetrofit(optimizelyToken)
-        val service: ExperimentsService = buildExperimentsService(authenticatedRetrofit)
-        val experimentsGenerator = ExperimentsGenerator()
+        val experimentsService: ExperimentsService = buildExperimentsService(authenticatedRetrofit)
+        val featuresService: FeaturesService = buildFeaturesService(authenticatedRetrofit)
+        val experimentsGenerator = ExperimentsGenerator(packageName)
+        val featuresGenerator = FeaturesGenerator(packageName)
         runBlocking {
-            val experiments = service.listExperiments(optimizelyProjectId)
+            val experiments = experimentsService.listExperiments(optimizelyProjectId)
             experimentsGenerator.generate(experiments)
+            val features = featuresService.listFeatures(optimizelyProjectId)
+            featuresGenerator.generate(features)
         }
     }
 }
