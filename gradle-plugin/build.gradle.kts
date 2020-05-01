2843669b7b4ff1dd2fb5bab9a3be907143e5b686
Patxi Bocos <patxi952@gmail.com>
2020-05-01 12:27:19 -0700

    Refactor in order to support Maven plugin (#40)
    * Refactoring into three modules
    
    * Workflows updated
    
    * Renaming core to generator

---
+++ gradle-plugin/build.gradle.kts
@@ -0,0 +1,78 @@
+import com.patxi.poetimizely.poetimizelyVersion
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "com.patxi"
+version = poetimizelyVersion(project)
+
+plugins {
+    `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "0.11.0"
+    jacoco
+    kotlin("jvm") version "1.3.72"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri(System.getenv("LOCAL_MAVEN_URL"))
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+
+    implementation("com.patxi:poetimizely-generator:1.0.0")
+    implementation("com.optimizely.ab:core-api:3.4.1")
+    implementation("com.squareup:kotlinpoet:1.5.0")
+    implementation("com.squareup.retrofit2:retrofit:2.7.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.7.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+
+    testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.2.7")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.5")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.5")
+    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation(gradleTestKit())
+}
+
+buildscript {
+    apply(file("gradle/ktlint.gradle.kts"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.test {
+    finalizedBy("jacocoTestReport")
+}
+
+gradlePlugin {
+    plugins {
+        create("poetimizely") {
+            id = "com.patxi.poetimizely"
+            displayName = "poetimizely"
+            description = "Generate Kotlin type safe accessors for Optimizely experiments and features"
+            implementationClass = "com.patxi.poetimizely.plugin.PoetimizelyPlugin"
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        xml.destination = file("${buildDir}/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/patxibocos/poetimizely"
+    vcsUrl = "https://github.com/patxibocos/poetimizely"
+    tags = listOf("optimizely", "kotlin", "typesafe", "kotlinpoet")
+}
---
+++ gradle-plugin/gradle/ktlint.gradle.kts
@@ -0,0 +1,22 @@
+val ktlint by configurations.creating
+
+dependencies {
+    ktlint("com.pinterest:ktlint:0.35.0")
+}
+
+tasks.register<JavaExec>("ktlint") {
+    ktlintSetup(group = "verification", description = "Check Kotlin code style.")
+    args("**/src/**/*.kt", "**/src/**/*.kts", "--relative")
+}
+
+tasks.register<JavaExec>("ktlintFormat") {
+    ktlintSetup(group = "formatting", description = "Fix Kotlin code style deviations.")
+    args("-F", "*.kt", "*.kts", "--relative")
+}
+
+fun JavaExec.ktlintSetup(group: String, description: String) {
+    this.group = group
+    this.description = description
+    this.classpath = ktlint
+    this.main = "com.pinterest.ktlint.Main"
+}
