96aba30e251e97c0ad64e85066da81eb1b406b34
Patxi Bocos <patxi952@gmail.com>
2021-01-09 13:07:41 +0100

    Updating some dependencies

--- gradle-plugin/gradle/ktlint.gradle.kts
+++ gradle-plugin/gradle/ktlint.gradle.kts
@@ -1,7 +1,7 @@
 val ktlint by configurations.creating
 
 dependencies {
-    ktlint("com.pinterest:ktlint:0.39.0")
+    ktlint("com.pinterest:ktlint:0.40.0")
 }
 
 tasks.register<JavaExec>("ktlint") {
